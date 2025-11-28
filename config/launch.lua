513f1b179c965e115cb987f1f910417bc8c7a812
David Pech <dpech@chemaxon.com>
2025-11-28 09:23:26 +0100

    Fish

--- config/launch.lua
+++ config/launch.lua
@@ -17,17 +17,17 @@ if platform.is_win then
       },
    }
 elseif platform.is_mac then
-   options.default_prog = { '/usr/local/bin/fish', '-l' }
+   options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { '/usr/local/bin/fish', '-l' } },
+      { label = 'Fish', args = { '/opt/homebrew/bin/fish', '-l' } },
       { label = 'Zsh', args = { 'zsh', '-l' } },
    }
 elseif platform.is_linux then
    options.default_prog = { 'fish', '-l' }
    options.launch_menu = {
       { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { 'fish', '-l' } },
+      { label = 'Fish', args = { '/opt/homebrew/fish', '-l' } },
       { label = 'Zsh', args = { 'zsh', '-l' } },
    }
 end
